777a3ac58b6b525cc5737d0d5f12b246e55ceca1
D'Souza <Jennifer.DSouza@tib.eu>
2026-08-12 21:29:06 +0200

    Switch documentation theme to Furo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,35 +8,42 @@
 
 import os
 import sys
+import tomllib
 
 sys.path.insert(0, os.path.abspath("../../"))
 
-project = "Schema-Minerpro"
+project = "Schema-Miner Pro"
 copyright = "2025, Sameer Sadruddin"
 author = "Sameer Sadruddin"
-release = "1.0.0"
+with open(os.path.abspath("../../pyproject.toml"), "rb") as pyproject:
+    release = tomllib.load(pyproject)["project"]["version"]
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx_rtd_theme", "sphinx.ext.autodoc"]
+extensions = ["sphinx.ext.autodoc"]
 
 templates_path = ["_templates"]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
+html_theme = "furo"
 html_static_path = ["_static"]
 
 html_theme_options = {
-    "logo_only": True,
-    "version_selector": True,
-    "language_selector": True,
-    "collapse_navigation": False,
-    "sticky_navigation": False,
-    "includehidden": True,
-    "titles_only": False,
+    "sidebar_hide_name": True,
+    "source_repository": "https://github.com/sciknoworg/schema-miner/",
+    "source_branch": "main",
+    "source_directory": "docs/source/",
+    "light_css_variables": {
+        "color-brand-primary": "#007c92",
+        "color-brand-content": "#005f73",
+    },
+    "dark_css_variables": {
+        "color-brand-primary": "#50d2df",
+        "color-brand-content": "#7de3ec",
+    },
 }
 
 html_css_files = [
